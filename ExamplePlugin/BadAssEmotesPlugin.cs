8e84272bb00b6d422452fc1db3ae6151a3fcba18
ToastedOven <49164897+ToastedOven@users.noreply.github.com>
2022-04-16 01:40:37 -0400

    wave 3 or somethin

--- ExamplePlugin/BadAssEmotesPlugin.cs
+++ ExamplePlugin/BadAssEmotesPlugin.cs
@@ -19,10 +19,11 @@ namespace ExamplePlugin
         public const string PluginGUID = "com.weliveinasociety.badassemotes";
         public const string PluginAuthor = "Nunchuk";
         public const string PluginName = "BadAssEmotes";
-        public const string PluginVersion = "1.1.1";
+        public const string PluginVersion = "1.2.0";
         public void Awake()
         {
             Assets.PopulateAssets();
+            Assets.AddSoundBank("nunchukemotes.bnk");
             Assets.AddSoundBank("BadAssEmotes.bnk");
             Assets.LoadSoundBanks();
             AddAnimation("Breakin", "Breakin_", false, true, true);
@@ -55,6 +56,9 @@ namespace ExamplePlugin
             AddAnimation("SpringLoaded", "SpringLoaded", true, false, false);
             AddAnimation("Springy", "Springy", true, true, true);
             AddAnimation("SquatKickIntro", "SquatKick", "SquatKickLoop", true, true);
+
+
+            //update 1
             AddAnimation("AnkhaZone", "AnkhaZone", true, true, true);
             AddAnimation("GangnamStyle", "GangnamStyle", true, true, true);
             AddAnimation("DontStart", "DontStart", true, true, true);
@@ -62,7 +66,7 @@ namespace ExamplePlugin
             AddAnimation("BestMates", "BestMates", true, true, true);
             AddAnimation("JackOPose", "", true, false, false);
             AddAnimation("Crackdown", "Crackdown", true, true, true);
-            AddAnimation("Thicc", "", true, false, false);
+            AddAnimation("Thicc", "Thicc", true, true, true);
             AddAnimation("TakeTheL", "TakeTheL", true, true, true);
             AddAnimation("ArkDance", "ArkDance", true, true, true);
             AddAnimation("LetsDanceBoys", "LetsDanceBoys", true, true, true);
@@ -73,12 +77,117 @@ namespace ExamplePlugin
             AddAnimation("Cartwheelin", "Cartwheelin", true, false, false);
             AddAnimation("CrazyFeet", "CrazyFeet", true, true, true);
             AddAnimation("FullTilt", "FullTilt", true, true, true);
-
             AddAnimation("FloorSamus", "FloorSamus", true, false, false);
             AddAnimation("DEDEDE", "DEDEDE", true, false, false);
-
             AddAnimation("Specialist", "Specialist", false, true, true);
+
+
+
+            //Update 2
+            AddStartAndJoinAnim(new string[] { "PPmusic", "PPmusicFollow" }, "PPmusic", true, true, true);
+            AddAnimation("GetDown", "GetDown", true, true, true);
+            AddAnimation("Yakuza", "Yakuza", true, true, true);
+            AddAnimation("Miku", "Miku", true, true, true);
+            AddAnimation("Horny", "Horny", true, true, true);
+            AddAnimation("GangTorture", "GangTorture", false, true, true);
+            AddAnimation("PoseBurter", "GinyuForce", true, true, true);
+            AddAnimation("PoseGinyu", "GinyuForce", true, true, true);
+            AddAnimation("PoseGuldo", "GinyuForce", true, true, true);
+            AddAnimation("PoseJeice", "GinyuForce", true, true, true);
+            AddAnimation("PoseRecoome", "GinyuForce", true, true, true);
+            AddStartAndJoinAnim(new string[] { "StandingHere", "StoodHere" }, "StandingHere", true, true, true);
+            AddAnimation("Carson", "Carson", false, true, true);
+            AddAnimation("Frolic", "Frolic", true, true, true);
+            AddAnimation("MoveIt", "MoveIt", true, true, true);
+            AddStartAndJoinAnim(new string[] { "ByTheFireLead", "ByTheFirePartner" }, "ByTheFire", true, true, true);
+            AddStartAndJoinAnim(new string[] { "SwayLead", "SwayPartner" }, "Sway", true, true, true);
+            AddAnimation("Macarena", "Macarena", true, true, true);
+            //AddAnimation("Renegade", "Renegade", true, true, true);
+            AddAnimation("Thanos", "Thanos", true, true, true);
+            AddAnimation("StarGet", "StarGet", false, false, false);
+            AddAnimation("Poyo", "Poyo", true, true, true);
+            AddAnimation("Hi", "Hi", false, false, false);
+            AddAnimation("Chika", "Chika", false, true, true);
+            AddAnimation("Goopie", "Goopie", false, true, true);
+            AddAnimation("Sad", "Sad", false, true, true);
+            AddAnimation("Crossbounce", "Crossbounce", true, true, true);
+            AddAnimation("Butt", "Butt", false, false, false);
+
+
+
+
+            //CustomEmotesAPI.AddCustomAnimation(new AnimationClip[] { Assets.Load<AnimationClip>($"@ExampleEmotePlugin_braindamage:assets/animationreplacements/Standing Here.anim"), Assets.Load<AnimationClip>($"@ExampleEmotePlugin_braindamage:assets/animationreplacements/I Realise.anim") }, true, "Play_StandingHere", "Stop_StandingHere", dimWhenClose: true, syncAnim: true, syncAudio: true, startPref: 0, joinPref: 1);
+
+            CustomEmotesAPI.animChanged += CustomEmotesAPI_animChanged;
         }
+        int stand = -1;
+        int prop1 = -1;
+        CameraTargetParams.CameraParamsOverrideHandle fovHandle;
+        internal CharacterBody localBody = null;
+        private void CustomEmotesAPI_animChanged(string newAnimation, BoneMapper mapper)
+        {
+            if (prop1 != -1)
+            {
+                prop1 = -1;
+            }
+            if (newAnimation == "StandingHere" )
+            {
+                stand = mapper.currentClip.syncPos;
+            }
+            if (stand != -1)
+            {
+                if (CustomAnimationClip.syncPlayerCount[stand] > 1)
+                {
+                    AkSoundEngine.SetRTPCValue("MetalGear_Vocals", 1);
+                }
+                else
+                {
+                    AkSoundEngine.SetRTPCValue("MetalGear_Vocals", 0);
+                }
+            }
+            if (newAnimation == "Chika")
+            {
+                prop1 = mapper.props.Count; //this aint netowrked, should have a unique list of props for each mapper?
+                mapper.props.Add(GameObject.Instantiate(Assets.Load<GameObject>("@BadAssEmotes_badassemotes:assets/models/desker.prefab")));
+                mapper.props[prop1].transform.SetParent(mapper.transform.parent);
+                mapper.props[prop1].transform.localEulerAngles = Vector3.zero;
+                mapper.props[prop1].transform.localPosition = Vector3.zero;
+
+                if (mapper.local)
+                {
+                    localBody = NetworkUser.readOnlyLocalPlayersList[0].master?.GetBody();
+                    CharacterCameraParamsData data = new CharacterCameraParamsData();
+                    data.fov = 70f;
+                    data.idealLocalCameraPos = new Vector3(0, 1.5f, -23);
+                    if (!fovHandle.isValid)
+                    {
+                        fovHandle = localBody.GetComponentInChildren<EntityStateMachine>().commonComponents.cameraTargetParams.AddParamsOverride(new CameraTargetParams.CameraParamsOverrideRequest
+                        {
+                            cameraParamsData = data
+                        }, 1f);
+                    }
+                }
+            }
+            else
+            {
+                if (mapper.local && fovHandle.isValid)
+                {
+                    localBody = NetworkUser.readOnlyLocalPlayersList[0].master?.GetBody();
+                    localBody.GetComponentInChildren<EntityStateMachine>().commonComponents.cameraTargetParams.RemoveParamsOverride(fovHandle, 1f);
+                    fovHandle = default(CameraTargetParams.CameraParamsOverrideHandle);
+                }
+            }
+            //if (newAnimation == "Sad")
+            //{
+            //    prop1 = mapper.props.Count;
+            //    mapper.props.Add(GameObject.Instantiate(Assets.Load<GameObject>("@BadAssEmotes_badassemotes:assets/models/trombone.prefab")));
+            //    mapper.props[prop1].transform.SetParent(mapper.a2.GetBoneTransform(HumanBodyBones.RightHand));
+            //    mapper.props[prop1].transform.localEulerAngles = new Vector3(0, 270, 0);
+            //    mapper.props[prop1].transform.localPosition = Vector3.zero;
+            //    mapper.props[prop1].transform.localScale = Vector3.one;
+            //}
+        }
+
         internal void AddAnimation(string AnimClip, string wwise, bool looping, bool dimAudio, bool sync)
         {
             CustomEmotesAPI.AddCustomAnimation(Assets.Load<AnimationClip>($"@ExampleEmotePlugin_badassemotes:assets/badassemotes/{AnimClip}.anim"), looping, $"Play_{wwise}", $"Stop_{wwise}", dimWhenClose: dimAudio, syncAnim: sync, syncAudio: sync);
@@ -86,6 +195,10 @@ namespace ExamplePlugin
         internal void AddAnimation(string AnimClip, string wwise, string AnimClip2ElectricBoogaloo, bool dimAudio, bool sync)
         {
             CustomEmotesAPI.AddCustomAnimation(Assets.Load<AnimationClip>($"@ExampleEmotePlugin_badassemotes:assets/badassemotes/{AnimClip}.anim"), false, $"Play_{wwise}", $"Stop_{wwise}", secondaryAnimation: Assets.Load<AnimationClip>($"@ExampleEmotePlugin_badassemotes:assets/badassemotes/{AnimClip2ElectricBoogaloo}.anim"), dimWhenClose: dimAudio, syncAnim: sync, syncAudio: sync);
+        }
+        internal void AddStartAndJoinAnim(string[] AnimClip, string wwise, bool looping, bool dimaudio, bool sync)
+        {
+            CustomEmotesAPI.AddCustomAnimation(new AnimationClip[] { Assets.Load<AnimationClip>($"@ExampleEmotePlugin_badassemotes:assets/badassemotes/{AnimClip[0]}.anim"), Assets.Load<AnimationClip>($"@ExampleEmotePlugin_badassemotes:assets/badassemotes/{AnimClip[1]}.anim") }, looping, $"Play_{wwise}", $"Stop_{wwise}", dimWhenClose: dimaudio, syncAnim: sync, syncAudio: sync, startPref: 1, joinPref: 0);
         }
     }
 }
