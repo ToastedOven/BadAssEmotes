74626f1e2bb2556b1c96fa2ca305bd37d721e28f
Brynzananas <brynzananas@gmail.com>
2024-10-07 20:26:34 +0600

    EnemyEmotes

--- ExamplePlugin/Settings.cs
+++ ExamplePlugin/Settings.cs
@@ -13,12 +13,17 @@ namespace ExamplePlugin
     public static class Settings
     {
         public static ConfigEntry<float> EnemyTauntOnDeathChance;
+        public static ConfigEntry<string> EnemyEmotes;
         internal static void Setup()
         {
             ModSettingsManager.SetModIcon(Assets.Load<Sprite>("@BadAssEmotes_badassemotes:assets/hydrolic/icon.png"));
             ModSettingsManager.SetModDescription("Don't ask me how I know");
             EnemyTauntOnDeathChance = BadAssEmotesPlugin.instance.Config.Bind<float>("Yes", "Enemies taunt on loss chance", 100, "Bottom Text");
             ModSettingsManager.AddOption(new SliderOption(EnemyTauntOnDeathChance));
+            EnemyEmotes = Config.Bind<string>("Yes",
+                             "Enemy Emotes",
+                             "Extraterrestial,Droop,SeeTinh,PopLock,DanceMoves,ImDiamond,Frolic,SwayLead,BestMates,Crackdown,Distraction,GangnamStyle,FlamencoIntro,Popular Vibe,VSWORLD,PPmusic,SquatKickIntro,Breakneck,Dougie,MyWorld,BimBamBom,GetDown,ArkDance,Macarena,ElectroSwing,Horny,Fresh,Goopie,TakeTheL,Infectious,Rollie,NeverGonna,CaliforniaGirls",
+                             "Emotes that enemies play on players death");
 
         }
     }
